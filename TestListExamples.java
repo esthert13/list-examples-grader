8bf643cdda74e659f63852c41dfa0fa61b72c17c
esthert13 <104547315+esthert13@users.noreply.github.com>
2022-11-10 23:25:28 -0800

    updated

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test
+  public void testMerge() {
+      List<String> combined = new ArrayList<>(List.of("Apple", "Bunny", "Calculator", "Disaster"));
+      List<String> list1 = new ArrayList<>(List.of("Apple", "Bunny"));
+      List<String> list2 = new ArrayList<>(List.of("Calculator", "Disaster"));
+      assertEquals(combined, ListExamples.merge(list1, list2));
+  }
+
+  @Test
+  public void testFilter() {
+      List<String> combined2 = new ArrayList<>(List.of("ancient", "banned", "believable", "disease"));
+      List<String> list1 = new ArrayList<>(List.of("ancient", "believable"));
+      List<String> list2 = new ArrayList<>(List.of("banned", "disease"));
+      assertEquals(combined2, ListExamples.merge(list1, list2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,26 @@
 # Create your grading script here
-
-set -e
+# set -e
+CPATH= ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
 
+if [[-f ListExamples]]
+then
+    echo "Passed: Correct file submitted"
+else
+    echo "Failed: Incorrect file submitted OR no file submitted"
+    exit 1
+fi
+
+javac -cp $CPATH *.java #2> out-err.txt >out.txt
+
+if [[$? -eq 0]]
+then
+    echo "Passed: Compile succeeded"
+else
+    echo "Failed: Your program didn't compile"
+    exit 2
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore GradeTests
